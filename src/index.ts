2cd2f9d59c55d1cd4f632d87c8ee1d006f960282
Francisco He Chi <fhchi2003@gmail.com>
2026-05-30 14:11:50 +0200

    Añadido dependencia para la conexión de frontend

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 import authRoutes from './routes/auth.routes';
 
-// 1. Cargar el .env PRIMERO
-dotenv.config();
-console.log("Comprobando inicio del servidor...");
-
-
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+// 2. Configurar CORS (Le damos permiso a frontend en el puerto 3000)
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json());
-
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
