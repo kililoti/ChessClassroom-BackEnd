be44a0456facc386387b245c287dd475d311fe6f
Francisco He Chi <fhchi2003@gmail.com>
2026-06-04 23:29:17 +0200

    Documentación routes para clase y autenticación

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,9 +3,9 @@ import { AuthController } from '../controllers/auth.controller';
 
 const router = Router();
 
-// Endpoint: POST /api/auth/registro
+// Registrar usuario Endpoint: POST /api/auth/registro
 router.post('/registro', AuthController.register);
 
-// Endpoint: POST /api/auth/login
+// Iniciar sesión Endpoint: POST /api/auth/login
 router.post('/login', AuthController.login);
 export default router;
--- src/routes/clase.routes.ts
+++ src/routes/clase.routes.ts
@@ -3,19 +3,25 @@ import { ClaseController } from '../controllers/clase.controller';
 
 const router = Router();
 
-// Creación y gestión
+// Creación Endpoint: POST /api/clases/crear
+// Estado Endpoint: PATCH /api/clases/:id/estado
 router.post('/crear', ClaseController.crearClase);
 router.patch('/:id/estado', ClaseController.alternarEstado);
 
-// Sistema de Invitaciones
+// Consultar información de clase Endpoint: GET /api/clases/invite/:codigo
+// Unirse a clase Endpoint: POST /api/clases/join
 router.get('/invite/:codigo', ClaseController.infoInvitacion); // Público: Muestra datos en la landing de invitación
 router.post('/join', ClaseController.unirseAClase); // Protegido: Ejecuta la acción de unirse
 
-// Borrar clase
+// Borrar clase Endpoint: DELETE /api/clases/:id
 router.delete('/:id', ClaseController.eliminarClase);
 
-// Listar clases por profesor o alumno
+// Listar clases por profesor Endpoint: GET /api/clases/profesor/:profesorId
+// Listar clases por alumno Endpoint: GET /api/clases/alumno/:alumnoId
 router.get('/profesor/:profesorId', ClaseController.listarClasesPorProfesor);
 router.get('/alumno/:alumnoId', ClaseController.listarClasesPorAlumno);
+
+// Consultar información de una clase específica (Para cargar el Dashboard de la clase y el Chat) Endpoint: GET /api/clases/:id
+router.get('/:id', ClaseController.obtenerClase); 
 
 export default router;
